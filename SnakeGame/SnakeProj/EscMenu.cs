653085987dd9fe14bdced7e212b8a12045eb3f8c
hen burshtein <85412921+henburshtein@users.noreply.github.com>
2022-04-28 00:55:30 +0300

    Add files via upload

---
+++ SnakeGame/SnakeProj/EscMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SnakeProj
+{
+    public partial class EscMenu : Form
+    {
+        public EscMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void EscMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 a = new Form2();
+            a.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 a = new Form2();
+            a.ShowDialog();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 a = new Form1();
+            a.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ SnakeGame/SnakeProj/Form2.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SnakeProj
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.move = new System.Windows.Forms.Timer(this.components);
+            this.moveup = new System.Windows.Forms.Timer(this.components);
+            this.movedown = new System.Windows.Forms.Timer(this.components);
+            this.moveleft = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Time = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // move
+            // 
+            this.move.Enabled = true;
+            this.move.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // moveup
+            // 
+            this.moveup.Enabled = true;
+            this.moveup.Tick += new System.EventHandler(this.moveup_Tick);
+            // 
+            // movedown
+            // 
+            this.movedown.Enabled = true;
+            this.movedown.Tick += new System.EventHandler(this.movedown_Tick);
+            // 
+            // moveleft
+            // 
+            this.moveleft.Enabled = true;
+            this.moveleft.Tick += new System.EventHandler(this.moveleft_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(19, 18);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // Time
+            // 
+            this.Time.Interval = 1000;
+            this.Time.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(251, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(633, 492);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "SnakeGame";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.snake);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form2_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer move;
+        public System.Windows.Forms.Timer moveup;
+        private System.Windows.Forms.Timer movedown;
+        private System.Windows.Forms.Timer moveleft;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer Time;
+        private System.Windows.Forms.Label label1;
+
+
+
+
+    }
+}
